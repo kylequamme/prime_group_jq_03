a31df475886e375d95b262a601dafc3673dcf653
Andrew Kolander <akolander22@gmail.com>
2016-06-29 16:19:30 -0500

    basic functions

---
+++ app.js
@@ -0,0 +1,61 @@
+$(function(){
+  var peopleArray = [{name: 'Brian A.', number: 0},
+                    {name: 'Richard', number: 0},
+                    {name: 'Justin', number: 0},
+                    {name: 'Donovan', number: 0},
+                    {name: 'Trent', number: 0},
+                    {name: 'Liz ', number: 0},
+                    {name: 'Andrew', number: 0},
+                    {name: 'Anthony', number: 0},
+                    {name: 'Hillary', number: 0},
+                    {name: 'Megan', number: 0},
+                    {name: 'Ryan', number: 0},
+                    {name: 'Connor', number: 0},
+                    {name: 'Kyle', number: 0},
+                    {name: 'Sahasha', number: 0},
+                    {name: 'Cormac', number: 0},
+                    {name: 'Brian V.', number: 0},
+                    {name: 'Tracy', number: 0},
+                    {name: 'Katie', number: 0},
+                    {name: 'Jon', number: 0},
+                    {name: 'Chewbaxter', number: 0}];
+var numTeams;
+
+function randomizer(array){
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+while (0 !== currentIndex) {
+  randomIndex = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
+
+  temporaryValue = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temporaryValue;
+  }
+return array;
+}
+
+function assignTeamNum(array, numTeams){
+  var num = 1;
+  for (var i = 0; i < array.length; i++) {
+    array[i].number = num;
+    num++;
+    if(num == numTeams+1){
+      num = 1;
+    }
+  }
+}
+
+$('.btn-group').on('click', function(event){
+  numTeams = parseInt(event.target.value);
+
+
+  $('#gen').removeClass('disabled');
+})
+
+$('#gen').on('click', function(){
+  assignTeamNum(randomizer(peopleArray), numTeams);
+  
+})
+});
