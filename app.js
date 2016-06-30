40ee2b5bd169b5bb7f1502eb613985c8f34b1365
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-06-30 12:42:18 -0500

    active button class

--- app.js
+++ app.js
@@ -22,59 +22,72 @@ $(function(){
   var numTeams;
   var tempTeamSize;
 
+  $('body').on('click', '#selectorButtons button', function (e) {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+  $('body').on('click', '#btn1 button', function (e) {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+  $('body').on('click', '#btn2 button', function (e) {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+
   $('#teamButton').on('click', function(){
-    $('.btn-numbers').hide();
-    $('#genGroup').addClass('disabled');
-    $('#genPeople').addClass('disabled');
+    $('#peopleButtons').hide();
+    $('#genGroup').css("display", "none");
+    // $('#genGroup').addClass('disabled');
     $('#groupButtons').css("display", "inline-block");
+
   })
   $('#peopleButton').on('click', function(){
-    $('.btn-numbers').hide();
+    $('#groupButtons').hide();
+    $('#genPeople').css("display", "none");
+    // $('#genPeople').addClass('disabled');
     $('#peopleButtons').css("display", "inline-block");
   })
 
   $('#groupButtons').on('click', function(event){
     numTeams = parseInt(event.target.value);
-    $('#genGroup').removeClass('disabled');
+    $('#genGroup').css("display", "inline-block");
   })
 
   $('#peopleButtons').on('click', function(event){
     tempTeamSize = parseInt(event.target.value);
-    $('#genPeople').removeClass('disabled');
+    $('#genPeople').css("display", "inline-block");
+  })
+
+  $('#genGroup').on('click', function(){
+    $('.groupList').empty();
+    $('#genGroup').css("display", "none");
+    assignTeamNum(randomizer(peopleArray), numTeams);
+    appendToDOM(peopleArray, numTeams);
   })
 
    $('#genPeople').on('click', function(){
      $('.groupList').empty();
      var tempTeamNum = parseInt(peopleArray.length/tempTeamSize);
-
      assignTeamNum(randomizer(peopleArray), tempTeamNum);
-     // console.log(peopleArray);
      appendToDOM(peopleArray, tempTeamNum);
-
    })
 
-  $('#genGroup').on('click', function(){
-    $('.groupList').empty();
-    assignTeamNum(randomizer(peopleArray), numTeams);
-    //console.log(peopleArray);
-    appendToDOM(peopleArray, numTeams);
 
-  })
 
 function randomizer(array){
   var currentIndex = array.length;
   var temporaryValue;
   var randomIndex;
-while (0 !== currentIndex) {
-  randomIndex = Math.floor(Math.random() * currentIndex);
-  currentIndex -= 1;
-
-  temporaryValue = array[currentIndex];
-  array[currentIndex] = array[randomIndex];
-  array[randomIndex] = temporaryValue;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-return array;
-}
 
 function assignTeamNum(array, numTeams){
   var num = 1;
@@ -84,7 +97,6 @@ function assignTeamNum(array, numTeams){
     if(num == numTeams+1){
       num = 1;
     }
-
   }
 }
 
